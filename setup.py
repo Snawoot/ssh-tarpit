fb9f6c2bb3cb336c3086c2ba611ccbb6bd77ad90
Vladislav Yarmak <vladislav@vm-0.com>
2019-03-23 21:52:37 +0200

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='ssh_tarpit',
-      version='0.1.4',
+      version='0.1.5',
       description="SSH tarpit that slowly sends an endless banner",
       url='https://github.com/Snawoot/ssh-tarpit',
       author='Vladislav Yarmak',
