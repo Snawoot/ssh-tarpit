5cbd1f00defd49b880e32b632c48565d0d760d74
Jeroen Hermans <jhermans@localhost.localdomain>
2021-03-14 13:48:23 +0100

    Space indentation

--- ssh_tarpit/utils.py
+++ ssh_tarpit/utils.py
@@ -13,13 +13,13 @@ def setup_logger(name, verbosity, logfile=False):
                                            '%Y-%m-%d %H:%M:%S'))
     logger.addHandler(handler)
     if logfile:
-      fh = logging.FileHandler(logfile)
-      fh.setLevel(level=verbosity)
-      fh.setFormatter(logging.Formatter('%(asctime)s '
-                                        '%(levelname)-8s '
-                                        '%(name)s: %(message)s',
-                                        '%Y-%m-%d %H:%M:%S'))
-      logger.addHandler(fh)
+        fh = logging.FileHandler(logfile)
+        fh.setLevel(level=verbosity)
+        fh.setFormatter(logging.Formatter('%(asctime)s '
+                                          '%(levelname)-8s '
+                                          '%(name)s: %(message)s',
+                                          '%Y-%m-%d %H:%M:%S'))
+        logger.addHandler(fh)
     return logger
 
 
